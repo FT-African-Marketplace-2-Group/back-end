db3e96079ccc4d885be359472636b6fb20841dca
sean-mahan <seanmahansean@gmail.com>
2021-10-18 07:14:56 -0500

    added base of user folder and other things

--- api/server.js
+++ api/server.js
@@ -1,35 +1,25 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
 
-function getAllUsers() { return db('users') }
-
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+const usersRouter = require("./users/users-router")
 
 const server = express()
+
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
-
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+server.use("/api/users", usersRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
-    message: err.message,
-    stack: err.stack,
+    /*For Security reasons this is commented out 
+    when not used in testing/Polishing */
+
+    //message: err.message,
+    //stack: err.stack,
+    clientMessage: err.clientMessage
   })
 })
 
